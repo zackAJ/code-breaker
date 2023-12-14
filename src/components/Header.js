7b939326f76549526e45da79a2083d5748bbe6cd
zackAJ <aktouf98zakaria@gmail.com>
2023-12-14 23:51:34 +0100

    Revert "testing"
    This reverts commit 41b45915c02368d6049bb81b5a687c29f24a8524.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,67 +1,21 @@
-"use client";
+'use client';
 /**
  * v0 by Vercel.
  * @see https://v0.dev/t/zLJBpfLIRuY
  */
 
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-
+import Drop from "@/components/Drop";
 export default function Component() {
-   useEffect(() => {
-			const handleBeforeInstallPrompt = (event) => {
-				event.preventDefault();
-				// Store the event for later use
-				// You can use this to trigger the installation prompt later
-				// e.g., when the user clicks a button
-				window.deferredPrompt = event;
-			};
-
-			window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-
-			return () => {
-				window.removeEventListener(
-					"beforeinstallprompt",
-					handleBeforeInstallPrompt
-				);
-			};
-		}, []);
-
-		const handleAddToHomeScreen = () => {
-			const { deferredPrompt } = window;
-
-			if (deferredPrompt) {
-				// Trigger the installation prompt
-				deferredPrompt.prompt();
-
-				// Wait for the user to respond to the prompt
-				deferredPrompt.userChoice.then((choiceResult) => {
-					if (choiceResult.outcome === "accepted") {
-						console.log("User accepted the A2HS prompt");
-					} else {
-						console.log("User dismissed the A2HS prompt");
-					}
-
-					// Reset the deferredPrompt
-					window.deferredPrompt = null;
-				});
-			}
-		};
 
 	return (
 		<header className="flex items-center justify-between px-4 py-2  shadow">
 			<div className="flex items-center">
 				<a href="/">
-					<img src="./logo.png" alt="logo" className="w-[50px]" />
+          <img src="./logo.png" alt="logo" className="w-[50px]"/>
 				</a>
 			</div>
 			<nav className="flex items-center space-x-4">
-				<button
-					className={`p-2 !rounded-md gradient-border`}
-					onClick={handleAddToHomeScreen}
-				>
-					إضافة إلى الشاشة الرئيسية
-				</button>
 				<Link className={`p-2 !rounded-md gradient-border`} href="/onePage">
 					الدليل 2
 				</Link>
